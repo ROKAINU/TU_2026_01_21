afb235d8efe8b10b44267e40798e8d7f400cc6c0
Rokainu <rokainu930@icloud.com>
2026-01-25 21:00:55 +0900

    画面外の判定

--- TU_20260121/Assets/Game/Script/IsInScreen.cs
+++ TU_20260121/Assets/Game/Script/IsInScreen.cs
@@ -5,7 +5,7 @@ public static class IsInScreen
     public static bool judge(Transform transform)
     {
         Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
-        return viewportPos.x >= 0f && viewportPos.x <= 1f &&
-                viewportPos.y >= 0f && viewportPos.y <= 1f;
+        return viewportPos.x >= -0.01f && viewportPos.x <= 1.01f &&
+                viewportPos.y >= -0.01f && viewportPos.y <= 1.01f;
     }
 }
